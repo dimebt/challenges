b5a488737c1e555387dca437384f0903fcc4c388
Dimitar Stefanovski <dimitar.stefanovski@netcetera.com>
2020-05-06 10:07:14 +0200

    Initial project setup

---
+++ DSF-Technical-education/DSF-Technical-education/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DSF-Technical-education
+//
+//  Created by Dimitar Stefanovski on 5/6/20.
+//  Copyright © 2020 Dimitar Stefanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
