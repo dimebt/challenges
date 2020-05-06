e7cd521d3990b7dd42982be92608d32668739f41
Dimitar Stefanovski <dimitar.stefanovski@netcetera.com>
2020-05-06 19:48:25 +0200

    challenge-03 completed

---
+++ Challenge-03-Autolayout/Challenge-03-Autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge-03-Autolayout
+//
+//  Created by Dimitar Stefanovski on 5/6/20.
+//  Copyright © 2020 Dimitar Stefanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
